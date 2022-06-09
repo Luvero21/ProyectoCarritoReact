dc60b82f00b7a8c7e033d03154f724e58e157153
luvero <luciana_lr@hotmail.com>
2022-06-08 21:13:24 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proximamente<code> :) :)</code> Carrito de compras
         </p>
         <a
           className="App-link"
