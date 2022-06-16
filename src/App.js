1a596e44406fe5fa90252267199a6e9779d753a4
luvero <luciana_lr@hotmail.com>
2022-06-15 23:10:29 -0300

    DesafioTres

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <ItemListContainer greeting="PROXIMAMENTE CARRITO"/>
+     <ItemListContainer greeting="PROXIMAMENTE CARRITO"/>
     </div>
   );
 }
